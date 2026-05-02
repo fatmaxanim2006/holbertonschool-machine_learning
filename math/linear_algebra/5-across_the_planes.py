0f97d191181419a9a5244a2df4c0f71e4fc5ea72
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-02 18:50:51 +0000

    Fix syntax error and clean code

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -4,20 +4,14 @@
 
 def add_matrices2D(mat1, mat2):
     """İki matrisi toplayır, ölçülər fərqlidirsə None qaytarır"""
-    # Sətir saylarını yoxla
-    if len(mat1) != len(mat2):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    
-    # Sütun saylarını yoxla (hər iki matrisin ilk sətirinə baxaraq)
-    if len(mat1[0]) != len(mat2[0]):
-        return None
-    
-    # Yeni matris yarat və cəmlə
+
     new_matrix = []
     for i in range(len(mat1)):
         row = []
         for j in range(len(mat1[i])):
             row.append(mat1[i][j] + mat2[i][j])
         new_matrix.append(row)
-        
-    return new_matrix:%s/\s\+$//e
+
+    return new_matrix
