17fa840d53622439543d9aed577e543d5e8e67d6
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-02 18:52:30 +0000

    Add task 6: array concatenation

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""İki siyahını birləşdirən funksiya"""
+
+
+def cat_arrays(arr1, arr2):
+    """İki siyahının birləşməsindən ibarət yeni siyahı qaytarır"""
+    return arr1 + arr2
