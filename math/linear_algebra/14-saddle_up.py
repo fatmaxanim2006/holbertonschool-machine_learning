e6ab7a539bb87f3fc5c0efbdf845c6df6ec546f0
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-02 19:08:31 +0000

    Add task 14: numpy matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""İki matrisi vuran funksiya"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Matris vurulmasını yerinə yetirir"""
+    return np.matmul(mat1, mat2)
