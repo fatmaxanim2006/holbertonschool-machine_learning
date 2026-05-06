a1e4c5a7c8195ec47d3d6417b6149914a3e9bb2f
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-06 17:41:23 +0000

    Add minor function

---
+++ math/advanced_linear_algebra/1-minor.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Matrisin minorlarını hesablayan modul"""
+
+
+def determinant(matrix):
+    """Matrisin determinantını hesablayan köməkçi funksiya"""
+    if matrix == [[]]:
+        return 1
+    n = len(matrix)
+    if n == 1:
+        return matrix[0][0]
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for c in range(n):
+        minor = [row[:c] + row[c+1:] for row in matrix[1:]]
+        det += ((-1) ** c) * matrix[0][c] * determinant(minor)
+    return det
+
+
+def minor(matrix):
+    """
+    Matrisin minor matrisini hesablayır.
+    """
+    if not isinstance(matrix, list) or \
+       not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if not matrix or not all(len(row) == len(matrix) for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    n = len(matrix)
+
+    # 1x1 matris xüsusi halı
+    if n == 1:
+        return [[1]]
+
+    minor_matrix = []
+    for i in range(n):
+        row_minors = []
+        for j in range(n):
+            # i-ci sətri və j-cu sütunu silərək sub-matrisi yaradırıq
+            sub_matrix = [row[:j] + row[j+1:] for row in
+                          (matrix[:i] + matrix[i+1:])]
+            # Həmin sub-matrisin determinantı bizim minorumuzdur
+            row_minors.append(determinant(sub_matrix))
+        minor_matrix.append(row_minors)
+
+    return minor_matrix
