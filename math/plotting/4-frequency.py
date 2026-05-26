bf504b7969f5b8fa21daf8e86934b6b56837b23f
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-26 20:42:41 +0000

    Fix: PEP8, documentation, and plot structure

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -12,10 +12,12 @@ def frequency():
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
+
     plt.figure(figsize=(6.4, 4.8))
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
+
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
     plt.xlim(0, 100)
-    plt.show()
+    plt.ylim(0, 30)
