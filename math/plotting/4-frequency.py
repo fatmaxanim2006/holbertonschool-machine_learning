3a9fbda17b955c1dd5588afea726a64002ae14a1
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-26 21:02:17 +0000

    Fixed docstrings, PEP 8, and plot parameters

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -12,23 +12,12 @@ def frequency():
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    
+
     plt.figure(figsize=(6.4, 4.8))
-    
-    # Histogramın parametrləri:
-    # bins: aralıqları 10 vahid artımla təyin edirik
-    # edgecolor: barların kənarlarını qara edir
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
-    
-    # Başlıq və oxların adları
+
     plt.title('Project A')
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
-    
-    # Oxların diapazonunu təyin etmək (istəyə bağlı, nümunəyə görə)
     plt.xlim(0, 100)
     plt.ylim(0, 30)
-    
-    # plt.show() - QEYD: Bir çox avtomatlaşdırılmış sistemlərdə
-    # plt.show() yazmaq testin səhv keçməsinə səbəb ola bilər, 
-    # tapşırıqdan asılı olaraq bunu silməyiniz lazım ola bilər.
