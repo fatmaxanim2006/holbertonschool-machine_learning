d92094f56f5b6250db64c09b6dd2794ce499f7a1
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-26 21:05:01 +0000

    Fixed docstrings, PEP 8, and plot parameters

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python3
-"""
-Bu modul tələbələrin ballarını göstərən histogramı çəkir.
-"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
-    """
-    Tələbələrin balları üçün histogramı çəkən funksiya.
-    """
+
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-
     plt.figure(figsize=(6.4, 4.8))
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
 
-    plt.title('Project A')
-    plt.xlabel('Grades')
-    plt.ylabel('Number of Students')
+    plt.hist(student_grades,
+             bins=range(0, 101, 10),
+             edgecolor='black')
+
     plt.xlim(0, 100)
     plt.ylim(0, 30)
+
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+
+    plt.show()
