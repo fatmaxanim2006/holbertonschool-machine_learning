406192da09404a79383f6fcd9ddee49d9c20d23d
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-26 20:28:03 +0000

    Fix: Add 0-line.py and README.md using vi

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -5,6 +5,6 @@ import matplotlib.pyplot as plt
 def line():
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(y, 'r-')
+    plt.plot(np.arange(0, 11), y, 'r-')
     plt.xlim(0, 10)
     plt.show()
---
+++ math/plotting/README.md
@@ -0,0 +1,2 @@
+# Math - Plotting
+Bu layihə riyazi funksiyaların qrafiklərini yaratmağı əhatə edir.
