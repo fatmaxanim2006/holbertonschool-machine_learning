0aeaaaf27b7672dad1ee30343c66c2f045404dee
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-26 20:30:48 +0000

    Fix: Added module and function docstrings

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+"""
+Module to plot a line graph
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """
+    Plots y as a line graph with x-axis ranging from 0 to 10
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(np.arange(0, 11), y, 'r-')
