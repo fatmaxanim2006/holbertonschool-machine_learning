a57341e92d53b38f3c9666663dabe9807348bc07
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-26 20:38:06 +0000

    Fix: Final adjustments to frequency histogram for PEP 8 and structure

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -13,15 +13,9 @@ def frequency():
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
-
-    # Histogramın yaradılması
     plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
-
-    # Etiketlər və başlıq
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-
-    # X oxunun diapazonu
     plt.xlim(0, 100)
     plt.show()
