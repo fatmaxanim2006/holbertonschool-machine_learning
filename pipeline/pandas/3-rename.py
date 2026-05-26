79cebb90276824471d4138d052468267afd2fa6e
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-05-26 18:45:57 +0000

    Fix trailing whitespace in blank lines

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -3,20 +3,12 @@
 Module to rename and convert columns in a DataFrame.
 """
 import pandas as pd
-
-
 def rename(df):
     """
     Renames the Timestamp column to Datetime, converts it to datetime objects,
     and returns a DataFrame with only Datetime and Close columns.
     """
-    # Sütun ismini değiştirme
     df = df.rename(columns={'Timestamp': 'Datetime'})
-    
-    # Unix timestamp'i datetime formatına dönüştürme
     df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
-    
-    # Sadece Datetime ve Close sütunlarını seçme
     df = df[['Datetime', 'Close']]
-    
     return df
