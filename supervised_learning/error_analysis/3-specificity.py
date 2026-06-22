6d20c0bbef7f7465a329f547e52afabdba5e84e9
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-22 18:31:58 +0000

    Fix pycodestyle whitespace errors in specificity

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -9,19 +9,9 @@ def specificity(confusion):
     confusion: numpy.ndarray of shape (classes, classes)
     Returns: numpy.ndarray of shape (classes,) containing the specificity
     """
-    # Her sınıf için TP, FP, FN, TN hesaplayalım
-    # TP: Diagonal elemanlar
     tp = np.diag(confusion)
-    
-    # FP: Sütun toplamları - TP
     fp = np.sum(confusion, axis=0) - tp
-    
-    # FN: Satır toplamları - TP
     fn = np.sum(confusion, axis=1) - tp
-    
-    # TN: Toplam - (TP + FP + FN)
     total = np.sum(confusion)
     tn = total - (tp + fp + fn)
-    
-    # Specificity = TN / (TN + FP)
     return tn / (tn + fp)
