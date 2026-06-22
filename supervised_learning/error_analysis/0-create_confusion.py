67984262dfbdd86d95f1cd54ee85cab8ee89fd93
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-22 18:20:36 +0000

    Create confusion matrix function and README

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
+"""Module to create a confusion matrix."""
 import numpy as np
+
 
 def create_confusion_matrix(labels, logits):
     """
-    Confusion matrix oluşturur.
-    labels: one-hot encoded (m, classes)
-    logits: one-hot encoded (m, classes)
+    Creates a confusion matrix.
+    labels: one-hot numpy.ndarray of shape (m, classes)
+    logits: one-hot numpy.ndarray of shape (m, classes)
+    Returns: a confusion numpy.ndarray of shape (classes, classes)
     """
-    # Her satırın sınıf indeksini bul (argmax kullanarak)
     true_labels = np.argmax(labels, axis=1)
     predicted_labels = np.argmax(logits, axis=1)
-    
-    # Sınıf sayısını al
+
     num_classes = labels.shape[1]
-    
-    # Matrisi oluştur
+
     confusion_matrix = np.zeros((num_classes, num_classes))
-    
-    # Matrisi doldur
+
     for i in range(len(true_labels)):
         confusion_matrix[true_labels[i], predicted_labels[i]] += 1
-        
+
     return confusion_matrix
