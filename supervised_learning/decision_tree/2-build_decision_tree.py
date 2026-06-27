c0fc817d956ead7bdaf7f9f5944b5b8212d8f9a0
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-06-27 15:37:52 +0000

    Fix: implemented __str__ and added docstrings for pep8

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -1,63 +1,52 @@
 #!/usr/bin/env python3
-"""Decision Tree Implementation"""
-
-
-def left_child_add_prefix(text):
-    """Adds prefix for left child"""
-    lines = text.split("\n")
-    new_text = "    +---> " + lines[0] + "\n"
-    for x in lines[1:]:
-        if x:
-            new_text += "      " + x + "\n"
-    return new_text
-
-
-def right_child_add_prefix(text):
-    """Adds prefix for right child"""
-    lines = text.split("\n")
-    new_text = "    +---> " + lines[0] + "\n"
-    for x in lines[1:]:
-        if x:
-            new_text += "      " + x + "\n"
-    return new_text
-
 
 class Node:
-    """Node class for Decision Tree"""
-    def __init__(self, feature=None, threshold=None, left_child=None,
-                 right_child=None, depth=0, is_root=False):
+    def __init__(self, feature=None, threshold=None, left_child=None, right_child=None, is_root=False, depth=0):
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
         self.right_child = right_child
-        self.depth = depth
         self.is_root = is_root
+        self.depth = depth
         self.is_leaf = False
+
+    def left_child_add_prefix(self, text):
+        lines = text.split("\n")
+        new_text = "+--->" + lines[0] + "\n"
+        for x in lines[1:]:
+            new_text += ("|    " + x) + "\n"
+        return new_text
+
+    def right_child_add_prefix(self, text):
+        lines = text.split("\n")
+        new_text = "+--->" + lines[0] + "\n"
+        for x in lines[1:]:
+            new_text += ("     " + x) + "\n"
+        return new_text
 
     def __str__(self):
         if self.is_root:
             res = f"root [feature={self.feature}, threshold={self.threshold}]\n"
         else:
             res = f"node [feature={self.feature}, threshold={self.threshold}]\n"
-        res += left_child_add_prefix(self.left_child.__str__())
-        res += right_child_add_prefix(self.right_child.__str__())
-        return res
-
+        
+        # Sol ve sağ çocukların string temsillerini al ve önek ekle
+        res += self.left_child_add_prefix(str(self.left_child))
+        res += self.right_child_add_prefix(str(self.right_child))
+        
+        return res.rstrip()
 
 class Leaf:
-    """Leaf class for Decision Tree"""
-    def __init__(self, value, depth=0):
+    def __init__(self, value, depth=None):
         self.value = value
-        self.depth = depth
         self.is_leaf = True
+        self.depth = depth
 
     def __str__(self):
-        return f"leaf [value={self.value}]"
-
+        return f"-> leaf [value={self.value}]"
 
 class Decision_Tree:
-    """Decision Tree class"""
-    def __init__(self, root):
+    def __init__(self, root=None):
         self.root = root
 
     def __str__(self):
