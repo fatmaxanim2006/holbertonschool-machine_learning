6d5f019cf1e7dac0eac85808972a5212f54074d2
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-04 18:14:37 +0000

    Add 1-neuron.py with private attributes and getters

---
+++ 1-neuron.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+This module defines a single neuron for binary classification with private attributes.
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Defines a single neuron performing binary classification.
+    """
+    def __init__(self, nx):
+        """
+        Initializes the neuron.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        
+        # Private instance attributes
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights vector"""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias"""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output"""
+        return self.__A
