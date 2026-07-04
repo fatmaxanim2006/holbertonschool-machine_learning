10d8ec53a4d9c49c09b735301be270712518a33c
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-04 17:34:05 +0000

    Fix pycodestyle errors

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -9,8 +9,6 @@ class Neuron:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        
+    <-- burada gizli boşluq var (12-ci sətir)
         self.nx = nx
-        self.W = np.random.randn(1, nx)
-        self.b = 0
-        self.A = 0
+        ...
