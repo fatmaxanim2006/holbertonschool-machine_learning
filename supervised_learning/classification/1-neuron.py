51e0c75cfd5d8c698a967f28a80a2a6f562e9796
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-04 18:17:23 +0000

    Add 1-neuron.py

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
-"""
-This module defines a single neuron for binary classification with private attributes.
-"""
+"""Neuron module"""
 import numpy as np
 
 
 class Neuron:
-    """
-    Defines a single neuron performing binary classification.
-    """
+    """Defines a single neuron performing binary classification"""
     def __init__(self, nx):
-        """
-        Initializes the neuron.
-        """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        
-        # Private instance attributes
-        self.__W = np.random.normal(size=(1, nx))
+
+        self.__W = np.random.randn(1, nx)
         self.__b = 0
         self.__A = 0
 
