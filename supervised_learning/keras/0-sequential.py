d84b2c95aca8886b2564a5fa95728266063ae1c9
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-10 15:20:55 +0000

    Add 0-sequential task

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Modul 0-sequential """
+import tensorflow.keras as K
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """ Neyron şəbəkəsini qurur """
+    model = K.models.Sequential()
+    regularizer = K.regularizers.l2(lambtha)
+    
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(K.layers.Dense(layers[i], activation=activations[i],
+                                     kernel_regularizer=regularizer,
+                                     input_shape=(nx,)))
+        else:
+            model.add(K.layers.Dense(layers[i], activation=activations[i],
+                                     kernel_regularizer=regularizer))
+        
+        if i < len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
+            
+    return model
