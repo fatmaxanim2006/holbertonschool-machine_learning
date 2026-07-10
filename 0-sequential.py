7784bb9fe6b716cea6e747595c3063411b34158e
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-10 15:50:26 +0000

    Fix pycodestyle E302 - add blank line before function

--- 0-sequential.py
+++ 0-sequential.py
@@ -1,25 +1,21 @@
 #!/usr/bin/env python3
-"""
-This module contains the function build_model
-"""
+""" Modul 0-sequential """
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """
-    Builds a neural network with the Keras library
-    """
+    """ Neyron şəbəkəsini qurur """
     model = K.models.Sequential()
     regularizer = K.regularizers.l2(lambtha)
 
     for i in range(len(layers)):
         if i == 0:
             model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                     kernel_regularizer=regularizer,
-                                     input_shape=(nx,)))
+                                      kernel_regularizer=regularizer,
+                                      input_shape=(nx,)))
         else:
             model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                     kernel_regularizer=regularizer))
+                                      kernel_regularizer=regularizer))
 
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
