6f5b22c5f57720f5536d588042e2aeca8a747a84
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-10 15:30:29 +0000

    Fix structure and add documentation

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-""" Modul 0-sequential """
+"""
+This module contains the function build_model
+"""
 import tensorflow.keras as K
 
+
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """ Neyron şəbəkəsini qurur """
+    """
+    Builds a neural network with the Keras library
+    """
     model = K.models.Sequential()
     regularizer = K.regularizers.l2(lambtha)
-    
+
     for i in range(len(layers)):
         if i == 0:
             model.add(K.layers.Dense(layers[i], activation=activations[i],
@@ -15,8 +20,8 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         else:
             model.add(K.layers.Dense(layers[i], activation=activations[i],
                                      kernel_regularizer=regularizer))
-        
+
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
-            
+
     return model
