f9a9b2542520feb8e5e511cbde87d61150ebc846
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-10 15:55:26 +0000

    Fix pycodestyle E302 - add blank line before function

--- 0-sequential.py
+++ 0-sequential.py
@@ -11,11 +11,11 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     for i in range(len(layers)):
         if i == 0:
             model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                      kernel_regularizer=regularizer,
-                                      input_shape=(nx,)))
+                                     kernel_regularizer=regularizer,
+                                     input_shape=(nx,)))
         else:
             model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                      kernel_regularizer=regularizer))
+                                     kernel_regularizer=regularizer))
 
         if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
