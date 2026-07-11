449e33cd5002ab2463b6393c79f5a5944490a1cd
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-11 15:46:22 +0000

    Add 3-one_hot.py

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python3
-""" Modul 3-one_hot """
+"""Trains a model using mini-batch gradient descent"""
 import tensorflow.keras as K
 
 
-def one_hot(labels, classes=None):
-    """ Etiket vektorunu one-hot matrisinə çevirir """
-    one_hot_matrix = K.utils.to_categorical(labels, num_classes=classes)
+def train_model(network, data, labels, batch_size, epochs,
+                 verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent
 
-    return one_hot_matrix
+    network: the model to train
+    data: numpy.ndarray of shape (m, nx) containing the input data
+    labels: one-hot numpy.ndarray of shape (m, classes) containing
+        the labels of data
+    batch_size: size of the batch used for mini-batch gradient descent
+    epochs: number of passes through data for mini-batch gradient descent
+    verbose: boolean that determines if output should be printed
+        during training
+    shuffle: boolean that determines whether to shuffle the batches
+        every epoch
+
+    Returns: the History object generated after training the model
+    """
+    history = network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+
+    return history
