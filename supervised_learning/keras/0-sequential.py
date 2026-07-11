018e08be9a612f9985674a2187b1001d92022f37
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-11 15:17:11 +0000

    Fix pycodestyle E302 - add blank line before function

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python3
-""" Modul 0-sequential """
+"""Builds a neural network with the Keras library"""
 import tensorflow.keras as K
 
+
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """ Neyron şəbəkəsini qurur """
-    model = K.models.Sequential()
+    """
+    Builds a neural network with the Keras library
+
+    nx: number of input features to the network
+    layers: list containing the number of nodes in each layer
+    activations: list containing the activation functions for each layer
+    lambtha: L2 regularization parameter
+    keep_prob: probability that a node will be kept for dropout
+
+    Returns: the keras model
+    """
     regularizer = K.regularizers.l2(lambtha)
+
+    model = K.Sequential()
 
     for i in range(len(layers)):
         if i == 0:
-            model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                     kernel_regularizer=regularizer,
-                                     input_shape=(nx,)))
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer,
+                input_shape=(nx,)
+            ))
         else:
-            model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                     kernel_regularizer=regularizer))
+            model.add(K.layers.Dense(
+                layers[i],
+                activation=activations[i],
+                kernel_regularizer=regularizer
+            ))
 
-        if i < len(layers) - 1:
+        if i != len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
 
     return model
