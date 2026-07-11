510527bb0a1dcd300ee22767e12d699639e71ee4
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-11 15:40:29 +0000

    Add 2-optimize.py

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
-""" Modul 2-optimize """
+"""Sets up Adam optimization for a keras model"""
 import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
-    """ Keras modeli üçün Adam optimizasiyasını qurur """
+    """
+    Sets up Adam optimization for a keras model with categorical
+    crossentropy loss and accuracy metrics
+
+    network: the model to optimize
+    alpha: learning rate
+    beta1: first Adam optimization parameter
+    beta2: second Adam optimization parameter
+
+    Returns: None
+    """
     optimizer = K.optimizers.Adam(learning_rate=alpha,
                                    beta_1=beta1,
                                    beta_2=beta2)
+
     network.compile(optimizer=optimizer,
                      loss='categorical_crossentropy',
                      metrics=['accuracy'])
