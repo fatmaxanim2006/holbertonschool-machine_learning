b1910b9ee6b8493b8c70548409ebef643078b1c0
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-11 15:38:42 +0000

    Add 1-input.py

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,20 +1,34 @@
 #!/usr/bin/env python3
-""" Modul 1-input """
+"""Builds a neural network with the Keras library using the Input class"""
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """ Neyron şəbəkəsini Keras funksional API ilə qurur """
+    """
+    Builds a neural network with the Keras library
+
+    nx: number of input features to the network
+    layers: list containing the number of nodes in each layer
+    activations: list containing the activation functions for each layer
+    lambtha: L2 regularization parameter
+    keep_prob: probability that a node will be kept for dropout
+
+    Returns: the keras model
+    """
     regularizer = K.regularizers.l2(lambtha)
+
     inputs = K.Input(shape=(nx,))
+    x = inputs
 
-    x = K.layers.Dense(layers[0], activation=activations[0],
-                        kernel_regularizer=regularizer)(inputs)
+    for i in range(len(layers)):
+        x = K.layers.Dense(
+            layers[i],
+            activation=activations[i],
+            kernel_regularizer=regularizer
+        )(x)
 
-    for i in range(1, len(layers)):
-        x = K.layers.Dropout(1 - keep_prob)(x)
-        x = K.layers.Dense(layers[i], activation=activations[i],
-                            kernel_regularizer=regularizer)(x)
+        if i != len(layers) - 1:
+            x = K.layers.Dropout(1 - keep_prob)(x)
 
     model = K.Model(inputs=inputs, outputs=x)
 
