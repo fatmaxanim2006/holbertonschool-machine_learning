617b4aa7b51bad7edd98e2fd7768eaf86f19597c
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-13 17:05:27 +0000

    Add 1-l2_reg_gradient_descent.py

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,35 +1,39 @@
 #!/usr/bin/env python3
-""" Gradient Descent with L2 Regularization """
+"""Gradient descent with L2 regularization"""
 import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
-    Updates the weights and biases of a neural network using gradient descent
-    with L2 regularization.
+    Updates the weights and biases of a neural network using gradient
+    descent with L2 regularization
+
+    Y is a one-hot numpy.ndarray of shape (classes, m) with correct labels
+    weights is a dictionary of the weights and biases of the neural network
+    cache is a dictionary of the outputs of each layer of the neural network
+    alpha is the learning rate
+    lambtha is the L2 regularization parameter
+    L is the number of layers of the network
+
+    The neural network uses tanh activations on each layer except the
+    last, which uses a softmax activation
+    The weights and biases of the network are updated in place
     """
     m = Y.shape[1]
-    
-    # dz represents the derivative of the loss with respect to Z
-    # Start with the last layer (Softmax activation)
-    dz = cache[f'A{L}'] - Y
-    
-    # Iterate backwards from layer L to 1
-    for i in range(L, 0, -1):
-        A_prev = cache[f'A{i-1}']
-        W = weights[f'W{i}']
-        b = weights[f'b{i}']
-        
-        # Calculate gradients for weights and bias
-        dw = (1 / m) * np.matmul(dz, A_prev.T) + (lambtha / m) * W
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        
-        # Calculate dZ for the next iteration (previous layer)
-        if i > 1:
-            # Derivative of tanh is (1 - tanh^2)
-            # A_prev is the output of the layer, already tanh applied
-            dz = np.matmul(W.T, dz) * (1 - (A_prev ** 2))
-            
-        # Update weights and biases in place
-        weights[f'W{i}'] -= alpha * dw
-        weights[f'b{i}'] -= alpha * db
+    weights_copy = weights.copy()
+    dZ = cache['A' + str(L)] - Y
+
+    for layer in range(L, 0, -1):
+        A_prev = cache['A' + str(layer - 1)]
+        W = weights_copy['W' + str(layer)]
+        b = weights_copy['b' + str(layer)]
+
+        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+        if layer > 1:
+            dA_prev = np.matmul(W.T, dZ)
+            dZ = dA_prev * (1 - A_prev ** 2)
+
+        weights['W' + str(layer)] = W - alpha * dW
+        weights['b' + str(layer)] = b - alpha * db
