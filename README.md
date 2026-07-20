c1395d1b34b3369e7b68adda3d7d7d0739df6333
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-20 11:58:07 +0000

    Fix conv_forward and add proper README

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Holberton School - Machine Learning
+
+This repository contains projects completed as part of the Holberton
+School Machine Learning specialization, covering topics such as
+supervised learning, unsupervised learning, and reinforcement learning.
+
+## supervised_learning/cnn
+
+This directory covers Convolutional Neural Networks (CNNs), including:
+
+* Forward propagation over convolutional and pooling layers
+* Back propagation over convolutional and pooling layers
+* Building CNNs with Keras and TensorFlow
+
+### Files
+
+* `0-conv_forward.py` - Performs forward propagation over a
+  convolutional layer of a neural network.
--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,44 +1,72 @@
 #!/usr/bin/env python3
-"""Convolutional forward propagation module"""
+"""Module that performs forward propagation over a convolutional layer."""
 import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
-    """Performs forward propagation over a convolutional layer
-    of a neural network"""
+    """
+    Performs forward propagation over a convolutional layer of a
+    neural network.
+
+    Args:
+        A_prev: numpy.ndarray of shape (m, h_prev, w_prev, c_prev)
+            containing the output of the previous layer
+                m: the number of examples
+                h_prev: the height of the previous layer
+                w_prev: the width of the previous layer
+                c_prev: the number of channels in the previous layer
+        W: numpy.ndarray of shape (kh, kw, c_prev, c_new) containing
+            the kernels for the convolution
+                kh: the filter height
+                kw: the filter width
+                c_prev: the number of channels in the previous layer
+                c_new: the number of channels in the output
+        b: numpy.ndarray of shape (1, 1, 1, c_new) containing the
+            biases applied to the convolution
+        activation: an activation function applied to the convolution
+        padding: string that is either 'same' or 'valid', indicating
+            the type of padding used
+        stride: tuple of (sh, sw) containing the strides for the
+            convolution
+                sh: the stride for the height
+                sw: the stride for the width
+
+    Returns:
+        The output of the convolutional layer.
+    """
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, c_prev, c_new = W.shape
     sh, sw = stride
 
     if padding == "same":
-        ph = int(((h_prev - 1) * sh + kh - h_prev) / 2) + 1
-        pw = int(((w_prev - 1) * sw + kw - w_prev) / 2) + 1
+        ph = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
+        pw = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
     else:
         ph, pw = 0, 0
 
     A_prev_padded = np.pad(
         A_prev,
         ((0, 0), (ph, ph), (pw, pw), (0, 0)),
-        mode='constant',
-        constant_values=0
+        mode="constant",
+        constant_values=0,
     )
 
-    h_new = int((h_prev + 2 * ph - kh) / sh) + 1
-    w_new = int((w_prev + 2 * pw - kw) / sw) + 1
+    h_new = (h_prev + 2 * ph - kh) // sh + 1
+    w_new = (w_prev + 2 * pw - kw) // sw + 1
 
     Z = np.zeros((m, h_new, w_new, c_new))
 
     for i in range(h_new):
         for j in range(w_new):
             for k in range(c_new):
-                h_start = i * sh
-                h_end = h_start + kh
-                w_start = j * sw
-                w_end = w_start + kw
-
-                A_slice = A_prev_padded[:, h_start:h_end, w_start:w_end, :]
+                v_start = i * sh
+                v_end = v_start + kh
+                h_start = j * sw
+                h_end = h_start + kw
+                A_slice = A_prev_padded[:, v_start:v_end, h_start:h_end, :]
+                kernel = W[:, :, :, k]
                 Z[:, i, j, k] = np.sum(
-                    A_slice * W[:, :, :, k], axis=(1, 2, 3)
+                    A_slice * kernel, axis=(1, 2, 3)
                 ) + b[0, 0, 0, k]
 
     return activation(Z)
