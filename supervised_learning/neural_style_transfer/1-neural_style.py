67bf9382808869391a73cbc828a449b3af071f19
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-07-29 11:30:56 +0000

    Fix eager execution guard in NST load_model

--- supervised_learning/neural_style_transfer/1-neural_style.py
+++ supervised_learning/neural_style_transfer/1-neural_style.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Neural Style Transfer"""
+
 import numpy as np
 import tensorflow as tf
 
 
 class NST:
-    """Performs tasks for Neural Style Transfer"""
+    """Performs tasks for neural style transfer"""
 
     style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
                      'block4_conv1', 'block5_conv1']
@@ -15,13 +16,12 @@ class NST:
         """
         Class constructor
 
-        parameters:
-            style_image [numpy.ndarray with shape (h, w, 3)]:
-                image used as style reference
-            content_image [numpy.ndarray with shape (h, w, 3)]:
-                image used as content reference
-            alpha [float]: weight for content cost
-            beta [float]: weight for style cost
+        style_image - the image used as a style reference, stored as
+            a numpy.ndarray
+        content_image - the image used as a content reference, stored
+            as a numpy.ndarray
+        alpha - the weight for content cost
+        beta - the weight for style cost
         """
         if not isinstance(style_image, np.ndarray) or \
                 len(style_image.shape) != 3 or style_image.shape[2] != 3:
@@ -31,18 +31,18 @@ class NST:
                 len(content_image.shape) != 3 or content_image.shape[2] != 3:
             raise TypeError(
                 "content_image must be a numpy.ndarray with shape (h, w, 3)")
-        if (not isinstance(alpha, (float, int))) or alpha < 0:
+        if (not isinstance(alpha, (int, float))) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
-        if (not isinstance(beta, (float, int))) or beta < 0:
+        if (not isinstance(beta, (int, float))) or beta < 0:
             raise TypeError("beta must be a non-negative number")
 
-        tf.enable_eager_execution()
+        if hasattr(tf, 'enable_eager_execution'):
+            tf.enable_eager_execution()
 
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
         self.alpha = alpha
         self.beta = beta
-
         self.load_model()
 
     @staticmethod
@@ -51,12 +51,10 @@ class NST:
         Rescales an image such that its pixels values are between 0
         and 1 and its largest side is 512 pixels
 
-        parameters:
-            image [numpy.ndarray with shape (h, w, 3)]:
-                image to be scaled
+        image - a numpy.ndarray of shape (h, w, 3) containing the
+            image to be scaled
 
-        returns:
-            the scaled image
+        Returns: the scaled image
         """
         if not isinstance(image, np.ndarray) or \
                 len(image.shape) != 3 or image.shape[2] != 3:
@@ -64,49 +62,56 @@ class NST:
                 "image must be a numpy.ndarray with shape (h, w, 3)")
 
         h, w, _ = image.shape
-        if h > w:
-            h_new = 512
-            w_new = int(w * (512 / h))
-        else:
+        if w > h:
             w_new = 512
             h_new = int(h * (512 / w))
+        else:
+            h_new = 512
+            w_new = int(w * (512 / h))
 
-        image = image[tf.newaxis, :]
-        image = tf.image.resize_bicubic(image, (h_new, w_new))
+        image = np.expand_dims(image, axis=0)
+
+        if hasattr(tf.image, 'resize_bicubic'):
+            image = tf.image.resize_bicubic(image, (h_new, w_new))
+        else:
+            image = tf.image.resize(
+                image, (h_new, w_new), method='bicubic')
+
         image = tf.clip_by_value(image / 255, 0, 1)
 
         return image
 
     def load_model(self):
         """
-        Creates the model used to calculate cost from VGG19 Keras
-        base model
+        Creates the model used to calculate cost
 
-        Model's input should match VGG19 input
-        Model's output should be a list containing the outputs of
-        the model's style and content layers, in order
+        the model's input should be the same as the VGG19 input
+        the model's output should be a list containing the outputs
+            of the VGG19 layers listed in style_layers followed by
+            content_layer
+
         Saves the model in the instance attribute model
         """
         VGG19_model = tf.keras.applications.VGG19(
             include_top=False, weights='imagenet')
 
-        x = VGG19_model.input
+        VGG19_model.save("VGG19_base_model")
+        custom_objects = {'MaxPooling2D': tf.keras.layers.AveragePooling2D}
+        vgg = tf.keras.models.load_model(
+            "VGG19_base_model", custom_objects=custom_objects)
 
-        outputs = []
-        vgg_layers = {layer.name: layer.output for layer in
-                      VGG19_model.layers}
+        style_outputs = []
+        content_output = None
 
-        for layer in VGG19_model.layers:
+        for layer in vgg.layers:
             if layer.name in self.style_layers:
-                outputs.append(layer.output)
+                style_outputs.append(layer.output)
             if layer.name == self.content_layer:
-                outputs.append(layer.output)
-            if isinstance(layer, tf.keras.layers.MaxPooling2D):
-                layer.__class__ = tf.keras.layers.AveragePooling2D
+                content_output = layer.output
 
-        model = tf.keras.models.Model(VGG19_model.input, outputs)
+            layer.trainable = False
 
-        model_outputs = model(x)
-        model = tf.keras.models.Model(x, model_outputs)
+        outputs = style_outputs + [content_output]
 
+        model = tf.keras.models.Model(vgg.input, outputs)
         self.model = model
