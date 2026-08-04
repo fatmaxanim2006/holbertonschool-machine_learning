e80f2c4e2f2ca0c4342478b7998e8962ab489f2a
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-08-04 13:40:59 +0000

    0. Initialize NST class for neural style transfer

--- supervised_learning/neural_style_transfer/0-neural_style.py
+++ supervised_learning/neural_style_transfer/0-neural_style.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Neural Style Transfer"""
-
 import numpy as np
 import tensorflow as tf
 
@@ -16,28 +15,25 @@ class NST:
         """
         Class constructor
 
-        style_image - the image used as a style reference, stored as
-            a numpy.ndarray
-        content_image - the image used as a content reference, stored
-            as a numpy.ndarray
+        style_image - the image used as a style reference,
+            stored as a numpy.ndarray
+        content_image - the image used as a content reference,
+            stored as a numpy.ndarray
         alpha - the weight for content cost
         beta - the weight for style cost
         """
         if not isinstance(style_image, np.ndarray) or \
-                len(style_image.shape) != 3 or style_image.shape[2] != 3:
+                style_image.shape[-1] != 3 or style_image.ndim != 3:
             raise TypeError(
                 "style_image must be a numpy.ndarray with shape (h, w, 3)")
         if not isinstance(content_image, np.ndarray) or \
-                len(content_image.shape) != 3 or content_image.shape[2] != 3:
+                content_image.shape[-1] != 3 or content_image.ndim != 3:
             raise TypeError(
                 "content_image must be a numpy.ndarray with shape (h, w, 3)")
         if (not isinstance(alpha, (int, float))) or alpha < 0:
             raise TypeError("alpha must be a non-negative number")
         if (not isinstance(beta, (int, float))) or beta < 0:
             raise TypeError("beta must be a non-negative number")
-
-        if hasattr(tf, 'enable_eager_execution'):
-            tf.enable_eager_execution()
 
         self.style_image = self.scale_image(style_image)
         self.content_image = self.scale_image(content_image)
@@ -56,26 +52,21 @@ class NST:
         Returns: the scaled image
         """
         if not isinstance(image, np.ndarray) or \
-                len(image.shape) != 3 or image.shape[2] != 3:
+                image.shape[-1] != 3 or image.ndim != 3:
             raise TypeError(
                 "image must be a numpy.ndarray with shape (h, w, 3)")
 
         h, w, _ = image.shape
-        if w > h:
-            w_new = 512
-            h_new = int(h * (512 / w))
-        else:
+        if h > w:
             h_new = 512
             w_new = int(w * (512 / h))
-
-        image = np.expand_dims(image, axis=0)
-
-        if hasattr(tf.image, 'resize_bicubic'):
-            image = tf.image.resize_bicubic(image, (h_new, w_new))
         else:
-            image = tf.image.resize(
-                image, (h_new, w_new), method='bicubic')
+            w_new = 512
+            h_new = int(h * (512 / w))
 
+        image = image[tf.newaxis, :]
+        image = tf.image.resize(image, size=(h_new, w_new),
+                                 method='bicubic')
         image = tf.clip_by_value(image / 255, 0, 1)
 
         return image
