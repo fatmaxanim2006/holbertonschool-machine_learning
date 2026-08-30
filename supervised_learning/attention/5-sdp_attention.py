ba5397ce0728f21399ee6b4764653a6c67c1877f
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-08-30 20:41:24 +0000

    Add sdp_attention function for scaled dot product attention

---
+++ supervised_learning/attention/5-sdp_attention.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""Scaled Dot Product Attention"""
+import tensorflow as tf
+
+
+def sdp_attention(Q, K, V, mask=None):
+    """Calculates the scaled dot product attention
+
+    Args:
+        Q: tensor with its last two dimensions as (..., seq_len_q, dk)
+            containing the query matrix
+        K: tensor with its last two dimensions as (..., seq_len_v, dk)
+            containing the key matrix
+        V: tensor with its last two dimensions as (..., seq_len_v, dv)
+            containing the value matrix
+        mask: tensor that can be broadcast into
+            (..., seq_len_q, seq_len_v) containing the optional mask,
+            or defaulted to None
+            if mask is not None, multiply -1e9 to the mask and add it
+            to the scaled matrix multiplication
+
+    Returns:
+        output, weights
+            output: tensor with its last two dimensions as
+                (..., seq_len_q, dv) containing the scaled dot product
+                attention
+            weights: tensor with its last two dimensions as
+                (..., seq_len_q, seq_len_v) containing the attention
+                weights
+    """
+    matmul_qk = tf.matmul(Q, K, transpose_b=True)
+
+    dk = tf.cast(tf.shape(K)[-1], tf.float32)
+    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+
+    if mask is not None:
+        scaled_attention_logits += (mask * -1e9)
+
+    weights = tf.nn.softmax(scaled_attention_logits, axis=-1)
+
+    output = tf.matmul(weights, V)
+
+    return output, weights
--- supervised_learning/attention/README.md
+++ supervised_learning/attention/README.md
@@ -134,3 +134,25 @@ calculates the positional encoding for a transformer.
 - `dm` is the model depth
 - Returns: a `numpy.ndarray` of shape `(max_seq_len, dm)` containing
   the positional encoding vectors
+
+### 5-sdp_attention.py
+Contains the function `sdp_attention(Q, K, V, mask=None)` that
+calculates the scaled dot product attention.
+
+- `Q` is a tensor with its last two dimensions as
+  `(..., seq_len_q, dk)` containing the query matrix
+- `K` is a tensor with its last two dimensions as
+  `(..., seq_len_v, dk)` containing the key matrix
+- `V` is a tensor with its last two dimensions as
+  `(..., seq_len_v, dv)` containing the value matrix
+- `mask` is a tensor that can be broadcast into
+  `(..., seq_len_q, seq_len_v)` containing the optional mask, or
+  defaulted to `None`
+  - if `mask` is not `None`, `-1e9` is multiplied to the mask and
+    added to the scaled matrix multiplication
+- The preceding dimensions of `Q`, `K`, and `V` are the same
+- Returns: `output, weights`
+  - `output` a tensor with its last two dimensions as
+    `(..., seq_len_q, dv)` containing the scaled dot product attention
+  - `weights` a tensor with its last two dimensions as
+    `(..., seq_len_q, seq_len_v)` containing the attention weights
