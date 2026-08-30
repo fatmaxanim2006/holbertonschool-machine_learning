e64554c173a369d9add7e7db2db944b7a118b808
fatmaxanim2006 <bagiyevafatma5@gmail.com>
2026-08-30 18:35:32 +0000

    Fix pycodestyle in Word2Vec training function

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -3,8 +3,9 @@
 import gensim
 
 
-def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
-                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
+def word2vec_model(
+        sentences, vector_size=100, min_count=5, window=5,
+        negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """
     Creates, builds, and trains a gensim word2vec model.
 
@@ -25,17 +26,13 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     """
     sg = 0 if cbow else 1
 
-    model = gensim.models.Word2Vec(sentences=sentences,
-                                    vector_size=vector_size,
-                                    min_count=min_count,
-                                    window=window,
-                                    negative=negative,
-                                    sg=sg,
-                                    seed=seed,
-                                    workers=workers)
+    model = gensim.models.Word2Vec(
+        vector_size=vector_size, min_count=min_count, window=window,
+        negative=negative, sg=sg, seed=seed, workers=workers)
 
-    model.train(sentences,
-                total_examples=model.corpus_count,
-                epochs=epochs)
+    model.build_vocab(sentences)
+
+    model.train(
+        sentences, total_examples=model.corpus_count, epochs=epochs)
 
     return model
